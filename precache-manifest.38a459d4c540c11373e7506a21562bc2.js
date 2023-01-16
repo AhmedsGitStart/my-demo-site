b709019f27b38a9df14f5499053cf7c0eed022ca
AhmedsGitStart <skkhubaibahmed0111@gmail.com>
2023-01-16 12:21:42 +0530

    Updates

---
+++ precache-manifest.38a459d4c540c11373e7506a21562bc2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "e1603836ae52ed00e8f9",
+    "url": "/my-demo-site/static/css/main.7a1efe4e.chunk.css"
+  },
+  {
+    "revision": "e1603836ae52ed00e8f9",
+    "url": "/my-demo-site/static/js/main.e1603836.chunk.js"
+  },
+  {
+    "revision": "d8f5ba55c3b78c25f0a5",
+    "url": "/my-demo-site/static/js/1.d8f5ba55.chunk.js"
+  },
+  {
+    "revision": "8d1b5bbd678f7ecbc7a3",
+    "url": "/my-demo-site/static/css/2.dde462ed.chunk.css"
+  },
+  {
+    "revision": "8d1b5bbd678f7ecbc7a3",
+    "url": "/my-demo-site/static/js/2.8d1b5bbd.chunk.js"
+  },
+  {
+    "revision": "82ba803045e58d21776e",
+    "url": "/my-demo-site/static/js/runtime~main.82ba8030.js"
+  },
+  {
+    "revision": "f830b7076712e627f02e57091be62cb7",
+    "url": "/my-demo-site/index.html"
+  }
+];
